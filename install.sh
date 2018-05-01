055c11550ae3e86779e8bd34aa70211b7e2b2475
maralla <maralla.ai@gmail.com>
2018-05-01 11:52:39 +0800

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.vim/mysnippets
 
-pack install
+log() {
+    echo -e "\033[1m\033[32mINFO:\033[0m $1"
+}
+
+
+if [ -d "$HOME/.vim" ]
+then
+    ts=$(date +"%Y%m%d@%H:%M:%S")
+    log "Make backup of '\$HOME/.vim' to '\$HOME/.vim.$ts'"
+    mv $HOME/{.vim,.vim.$ts}
+fi
+
+log "Clone maralla/dotvim..."
+git clone https://github.com/maralla/dotvim.git $HOME/.vim
+log "Install packs..."
+(cd $HOME/.vim && pack install)
+log "Done"
